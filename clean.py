dacc29b0301053a25131b01a47a1e552d892a5e9
rokniraha1385@gmail.com <rokniraha1385@gmail.com>
2022-08-31 16:45:17 +0430

    clean.py v.2 added to git

--- clean.py
+++ clean.py
@@ -1,19 +1,19 @@
-from django.db import models
 
-# Create your models here.
 
-class Sender(models.Model):
+
+
+class Sender   (   models.Model)
     name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+   	 email   = models.CharField(max_length=200)
     imageUrl = models.CharField(max_length=250)
-    def __str__(self):
+    def __str__(self)
         return self.name
 
-class Message(models.Model):
+class Message(models.Model)
     sender = models.CharField(max_length=200)
-    recipient = models.CharField(max_length=200)
-    message = models.CharField(max_length=200)
+    recipient =			 models.CharField(max_length=200)
+    	message = models.CharField(max_length=200)
     visible = models.IntegerField(default=1)
-    timestamp = models.DateTimeField('date created')
-    def __str__(self):
-        return self.message
+    		timestamp = models.DateTimeField('date created')
+    def __str__(self)
+       		 return self.message
